c3a5d30ee397fce7a2b9e00d1e0c9e828fe9725a
Natalia <n.jarmakowska@student.maastrichtuniversity.nl>
2026-04-16 14:11:49 +0200

    Changed the name AlertGenerator.java to AlertManager.java to avoid any errors

---
+++ src/main/java/com/alerts/AlertManager.java
@@ -0,0 +1,53 @@
+package com.alerts;
+
+import com.data_management.DataStorage;
+import com.data_management.Patient;
+
+/**
+ * The {@code AlertGenerator} class is responsible for monitoring patient data
+ * and generating alerts when certain predefined conditions are met. This class
+ * relies on a {@link DataStorage} instance to access patient data and evaluate
+ * it against specific health criteria.
+ */
+public class AlertManager {
+// FIX: Made the dataStorage field final 
+    private final DataStorage dataStorage;
+
+    /**
+     * Constructs an {@code AlertGenerator} with a specified {@code DataStorage}.
+     * The {@code DataStorage} is used to retrieve patient data that this class
+     * will monitor and evaluate.
+     *
+     * @param dataStorage the data storage system that provides access to patient
+     *                    data
+     */
+    public AlertManager(DataStorage dataStorage) {
+        this.dataStorage = dataStorage;
+    }
+// FIX: Fixed the javadoc comment
+    /**
+     * Evaluates the specified patient's data to determine if any alert conditions
+     * are met. If a condition is met, an alert is triggered via the
+     * {@link #triggerAlert(Alert)}
+     * method. This method should define the specific conditions under which an alert
+     * will be triggered.
+     * @param patient the patient data to evaluate for alert conditions
+     */
+
+// FIX: Added to do comments to inform what needs to be implemented
+    public void evaluateData(Patient patient) {
+        // TODO: Implement the code to evaluate patient data
+    }
+
+    /**
+     * Triggers an alert for the monitoring system. This method can be extended to
+     * notify medical staff, log the alert, or perform other actions. The method
+     * currently assumes that the alert information is fully formed when passed as
+     * an argument.
+     *
+     * @param alert the alert object containing details about the alert condition
+     */
+    private void triggerAlert(Alert alert) {
+        // TODO: Implement the code to trigger an alert
+    }
+}
