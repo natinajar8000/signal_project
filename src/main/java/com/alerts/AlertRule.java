f795a5f935c639b75dd6694a2ebbcecb079d3707
Natalia <n.jarmakowska@student.maastrichtuniversity.nl>
2026-05-16 17:15:00 +0200

    Made alert logic depend on AlertStrategy interface and triggering alerts on AlertFactory class

--- src/main/java/com/alerts/AlertRule.java
+++
@@ -1,9 +0,0 @@
-package com.alerts;
-
-import com.data_management.Patient;
-import com.data_management.PatientRecord;
-import java.util.List;
-
-public interface AlertRule {
-    List<Alert> check(Patient patient, List<PatientRecord> patientRecords);
-}
