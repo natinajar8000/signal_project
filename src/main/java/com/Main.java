ac8db748e2474be708bee2e1cfe5484b8c2df286
Natalia <n.jarmakowska@student.maastrichtuniversity.nl>
2026-05-19 19:16:00 +0200

    Documented  the main class

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,8 +3,14 @@ package com;
 import com.data_management.DataStorage;
 import com.cardio_generator.HealthDataSimulator;
 
+// Main class of the application
 public class Main {
 
+    /**
+     * The main entry point of the application
+     * Opens the simulator or data storage
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         try {
             if (args.length > 0 && args[0].equalsIgnoreCase("DataStorage")) {
